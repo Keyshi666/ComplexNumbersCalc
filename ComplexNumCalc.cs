b1746052eac1201390efec8b2a55d350dbb84462
Omikronikus <johny9763@gmail.com>
2019-09-29 20:22:25 +0600

    Initial commit

---
+++ ComplexNumCalc.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Complexcalc
+{
+    class ComplexNumCalc
+    {
+        protected Complex a;
+        protected Complex b;
+
+        public ComplexNumCalc(Complex a, Complex b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public string Compare()
+        {
+            if (this.a.Equals(this.b))
+            {
+                return "=";
+            }
+            else if (this.a.Magnitude > this.b.Magnitude)
+            {
+                return ">";
+            }
+            else if (this.a.Magnitude < this.b.Magnitude)
+            {
+                return "<";
+            }
+            return "Infinity";
+        }
+
+        public Complex Add() 
+        {
+            return Complex.Add(this.a, this.b);
+        }
+
+        public Complex Multiply()
+        {
+            return Complex.Multiply(this.a, this.b);
+        }
+        public Complex Divide()
+        {
+            return Complex.Divide(this.a, this.b);
+        }
+        public Complex Subtract()
+        {
+            return Complex.Subtract(this.a, this.b);
+        }
+    }
+}
---
+++ UserControl1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace Complexcalc
+{
+    public partial class UserControl1: UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = new ComplexNumCalc(new Complex(1,2), new Complex(2, 2)).compare();
+        }
+    }
+}
